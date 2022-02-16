2357b3c5308ab7a99dd836623fd2a146e261ac97
xinyi chen <chen.xiny@northeastern.edu>
2022-02-16 05:40:00 -0500

    add auth token test file

---
+++ authorization.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+
+const secretKey = "secretKey";
+module.exports = {
+  secretKey : "secretKey"
+}
+// create token
+module.exports.generateToken = function (payload) { 
+  const token =
+    "Bearer " +
+    jwt.sign(payload, secretKey, {
+      expiresIn: 60 * 60,
+    });
+  return token;
+};
+
+// check token
+module.exports.verifyToken = function (req, res, next) {
+
+  if(req.headers.authorization == undefined){
+    return res.json({ code: "403", msg: "invalid token" })
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  console.log("verify=" + token)
+  jwt.verify(token, secretKey, function (err, decoded) {
+    if (err) {
+      console.log("verify error", err);
+      return res.json({ code: "400", msg: "invalid token" });
+    }
+  
+    next();
+  });
+};
