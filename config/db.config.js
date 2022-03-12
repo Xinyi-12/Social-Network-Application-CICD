0bdac086eb6140dd9ca2a12acdc4dd29424b6c74
xinyi chen <chen.xiny@northeastern.edu>
2022-03-12 11:47:31 -0500

    modified config/ packer

--- config/db.config.js
+++ config/db.config.js
@@ -4,11 +4,18 @@ const { createPool } = require("mysql");
 const db = createPool({
   port: 3306,
   host: "us-cdbr-east-05.cleardb.net",
-  user: "bd87f1a8bcc9a6",
-  password: "85bb7c74",
-  database: "heroku_61131b50aa1b192",
+  user: "csye622504",
+  password: "3205416536Cxy",
+  database: "csye622504",
   connectionLimit: 10,
 });
 /** Connection pool creation - END */
 
+db.connet((err) => {
+  if(err){
+    console.log(err.message);
+    return;
+  }
+  console.log("connected");
+})
 module.exports = db;
--- packer/buildAmi.sh
+++ packer/buildAmi.sh
@@ -2,17 +2,10 @@
 sleep 20
 # Install node js
 sudo yum install -y gcc-c++ make
-
 sudo yum install --assumeyes curl
 curl --silent --location https://rpm.nodesource.com/setup_14.x | sudo bash -
 sudo yum install -y nodejs
-
 node -v
 
 # Install pm2
 sudo npm install -g pm2
-
-sudo pm2 start ./index.js
-sudo pm2 startup systemd
-sudo pm2 save
-sudo pm2 list
