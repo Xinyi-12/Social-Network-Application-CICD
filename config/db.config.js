01149ca9e0ea393bd01114e20e5fe2bc91bfe745
Xinyi-12 <71161607+Xinyi-12@users.noreply.github.com>
2022-03-26 13:25:53 -0400

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -22,55 +22,55 @@ const db = createPool({
 });
 
 
-// db.getConnection(function (err) {
-//   if (err) throw err;
-//   console.log("Connected!");
+db.getConnection(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
 
-//   var drop1 = "DROP TABLE IF EXISTS `users`;"
-//   db.query(drop1, function (err, result) {
-//     if (err) throw err;
-//     console.log("drop1");
-//   });
-//   setTimeout(function () {
-//     var sql =
-//       "CREATE TABLE users(" +
-//       "`id` int(11) NOT NULL AUTO_INCREMENT," +
-//       "`firstName` varchar(45) NOT NULL," +
-//       "`lastName` varchar(45) NOT NULL," +
-//       "`emailId` varchar(45) NOT NULL," +
-//       "`password` varchar(100) NOT NULL," +
-//       "`account_created` datetime NOT NULL," +
-//       "`account_updated` datetime NOT NULL," +
-//       "PRIMARY KEY (`id`)," +
-//       "UNIQUE KEY `emailId_UNIQUE` (`emailId`))"
-//     db.query(sql, function (err, result) {
-//       if (err) throw err;
-//       console.log("users Table created");
-//     });
-//   }, 1000);
+  var drop1 = "DROP TABLE IF EXISTS `users`;"
+  db.query(drop1, function (err, result) {
+    if (err) throw err;
+    console.log("drop1");
+  });
+  setTimeout(function () {
+    var sql =
+      "CREATE TABLE users(" +
+      "`id` int(11) NOT NULL AUTO_INCREMENT," +
+      "`firstName` varchar(45) NOT NULL," +
+      "`lastName` varchar(45) NOT NULL," +
+      "`emailId` varchar(45) NOT NULL," +
+      "`password` varchar(100) NOT NULL," +
+      "`account_created` datetime NOT NULL," +
+      "`account_updated` datetime NOT NULL," +
+      "PRIMARY KEY (`id`)," +
+      "UNIQUE KEY `emailId_UNIQUE` (`emailId`))"
+    db.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("users Table created");
+    });
+  }, 1000);
 
 
-//   var drop2 = "DROP TABLE IF EXISTS `images`;"
-//   db.query(drop2, function (err, result) {
-//     if (err) throw err;
-//     console.log("drop2");
-//   });
-//   setTimeout(function () {
-//     var sql2 = "CREATE TABLE images(" +
-//       "`id` int(45) NOT NULL AUTO_INCREMENT," +
-//       "`description` varchar(1000) NOT NULL," +
-//       "`imagePath` varchar(1000) NOT NULL," +
-//       "`datetimeCreated` datetime NOT NULL," +
-//       "`addedByUserId` int(11) NOT NULL," +
-//       "PRIMARY KEY (`id`))"
+  var drop2 = "DROP TABLE IF EXISTS `images`;"
+  db.query(drop2, function (err, result) {
+    if (err) throw err;
+    console.log("drop2");
+  });
+  setTimeout(function () {
+    var sql2 = "CREATE TABLE images(" +
+      "`id` int(45) NOT NULL AUTO_INCREMENT," +
+      "`description` varchar(1000) NOT NULL," +
+      "`imagePath` varchar(1000) NOT NULL," +
+      "`datetimeCreated` datetime NOT NULL," +
+      "`addedByUserId` int(11) NOT NULL," +
+      "PRIMARY KEY (`id`))"
 
-//     db.query(sql2, function (err, result) {
-//       if (err) throw err;
-//       console.log("images Table created");
-//     });
-//   }, 1000);
+    db.query(sql2, function (err, result) {
+      if (err) throw err;
+      console.log("images Table created");
+    });
+  }, 1000);
 
-// });
+});
 
 module.exports = db;
 
