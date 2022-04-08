e5a0f655f5f920136fd4bfcc43e5b9a3f76a7f16
xinyi chen <chen.xiny@northeastern.edu>
2022-04-08 14:38:33 -0400

    edit hook

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 sudo echo 'export PATH=$PATH:/home/ec2-user/node_modules/pm2/bin' | sudo tee -a source ~/.bash_profile
-sudo rm -rf /home/ec2-user/.pm2
-source ~/.bash_profile
-pm2 flush 
-pm2 stop all
-pm2 delete all
-cd /home/ec2-user/
-# ls
+
+cd /home/ec2-user
+sudo npm -i
 pm2 start index.js
 
