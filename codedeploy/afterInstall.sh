daed9d82d84c3874262614696cf3166966fc5146
xinyi chen <chen.xiny@northeastern.edu>
2022-04-08 12:47:32 -0400

    edit hook

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-source ~/.bash_profile
+sudo chmod -R 777 /home/ec2-user
+cd ~
+cd src
+sudo yum update -y
+sudo yum install -y gcc-c++ make
+sudo yum install --assumeyes curl
+curl --silent --location https://rpm.nodesource.com/setup_14.x | sudo bash -
+sudo yum install -y nodejs
+node -v
+sleep 10
+npm cache clean --force
+npm update
+
 # sudo mv -f /home/ec2-user/config.js /home/ec2-user/model/
-cd /home/ec2-user/
-sudo chmod 775 /home/ec2-user/node_modules
-npm -f install
-sudo rm -rf /home/ec2-user/.pm2
+# cd /home/ec2-user/
+# sudo chmod 775 /home/ec2-user/node_modules
+# npm -f install
+# sudo rm -rf /home/ec2-user/.pm2
