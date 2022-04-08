0714c943ca72fd1cc81f5cde0be074ab2404290e
xinyi chen <chen.xiny@northeastern.edu>
2022-04-08 01:50:35 -0400

    load agent

--- packer/buildAmi.sh
+++ packer/buildAmi.sh
@@ -6,14 +6,19 @@ sudo yum install --assumeyes curl
 curl --silent --location https://rpm.nodesource.com/setup_14.x | sudo bash -
 sudo yum install -y nodejs
 node -v
+sudo yum install ruby
+sudo yum install wget
+CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
+sudo yum erase codedeploy-agent -y
+sudo cd /home/ec2-user
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+sudo chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent status
+sudo service codedeploy-agent start
+sudo service codedeploy-agent status
 
 # Install pm2
 sudo yum update -y
 npm install
 sudo npm install -g pm2
-# cd /home/ec2-user
-# sudo pm2 start ./index.js
-# sudo pm2 startup systemd
-# sudo pm2 save
-# sudo pm2 list
-
