27e6c60781a5b0457f9cb2838bdacc6c82c13080
Xinyi-12 <71161607+Xinyi-12@users.noreply.github.com>
2022-04-10 21:46:51 -0400

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -78,38 +78,7 @@ exports.register =  (req, res, next) => {
         const tokgen = new TokenGenerator(256, TokenGenerator.BASE62);
 
         const uuid = tokgen.generate();
-        // const character = {
-
-        //     username: req.body.emailId,
-        //     token: uuid,
-        //     ttl: Math.floor(Date.now() / 1000) + 2 * 60
-
-        // }
-        // console.log(character)
-
-        // const dyParams = {
-        //     TableName: 'csye6225',
-        //     Item: character
-        // }
-
-        // await docClient.put(dyParams).promise();
-
-
-        // const params = {
-        //     //Protocol:'lambda',
-        //     Message: req.body.emailId + ',' + uuid,
-        //     TopicArn: 'arn:aws:sns:us-east-1:696912630749:verification',
-        //     //Endpoint:'arn:aws:lambda:us-east-1:444584272403:function:testlambda'
-        // }
-        // console.log(params);
-
-        // await new AWS.SNS({
-        //     apiVersion: '2010-03-31'
-        // }).publish(params).promise();
-
-        // console.log("sns.......");
-
-        //bcrypt.hashSync(req.body.password, 10)
+      
         console.log(data.password);
 
 
@@ -133,9 +102,6 @@ exports.register =  (req, res, next) => {
             })
         })
 
-    // } catch (err) {
-    //     res.status(500).json(err)
-    // }
 
 };
 
@@ -561,231 +527,14 @@ exports.deleteProfilePic = (req, res, next) => {
 
 }
 
-// {
-//     "file_name": "image.jpg",
-//     "id": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-//     "url": "bucket-name/user-id/image-file.extension",
-//     "upload_date": "2020-01-12",
-//     "user_id": "d290f1ee-6c54-4b01-90e6-d701748f0851"
-//   }
-
-
-// export const create = async (req, res) => {
-//     if (!req.body.emailId || !req.body.password || !req.body.firstName || !req.body.lastName) {
-//         res.status(400).send({
-//             message: "Input can not be empty!"
-//         });
-//         return;
-//     }
-
-
-//     var expires = new Date();
-//     // expires.setTime(expires.getTime()+60*2*1000)
-//     const regEmail = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
-//     let isEmail = regEmail.test(req.body.emailId);
-//     if (!isEmail) {
-//         res.status(400).json(
-//             "Username must be an email!"
-//         );
-//         return;
-//     }
-
-
-//     try {
-//         const userExist = await userDB.findOne({
-//             attributes: {
-//                 exclude: ['password']
-//             },
-//             where: {
-//                 username: req.body.username
-//             }
-//         })
-//         const uuid = v4();
-//         if (userExist != null && userExist.verify == true) {
-//             res.status(400).json("Username has been used");
-//             return
-//         }
-//         if (userExist != null && userExist.verify == false) {
-
-//             var param = {
-//                 TableName: 'csye6225',
-//                 Key: {
-//                     'username': {
-//                         S: userExist.username
-//                     }
-//                 }
-//             };
-
-//             ddb.getItem(param, (err, data) => {
-
-
-//                 console.log(data.Item)
-//                 if (data.Item === undefined) {
-
-//                     const character = {
-
-//                         username: req.body.username,
-//                         token: uuid,
-//                         ttl: Math.floor(Date.now() / 1000) + 2 * 60
-
-//                     }
-//                     const dyParams = {
-//                         TableName: tableName,
-//                         Item: character
-//                     }
-
-//                     docClient.put(dyParams).promise()
 
 
 
-//                     const params = {
-
-//                         Message: req.body.username + ',' + uuid,
-//                         TopicArn: 'arn:aws:sns:us-east-1:971613862138:verification',
-
-//                     }
-
-
-//                     new AWS.SNS({
-//                         apiVersion: '2010-03-31'
-//                     }).publish(params).promise();
-//                     res.status(200).json("Please check your email to verify!");
-//                     return;
-
-//                 } else {
-
-//                     res.status(400).json("Please login in your email to verify your account!");
-//                     return;
-//                 }
-//             }
-
-//             )
-//             return;
-//         }
-
-//         //Generate an encrypted password
-//         const salt = await bcrypt.genSalt(10);
-//         const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-
-//         const user = {
-//             username: req.body.username,
-//             password: hashedPassword,
-//             firstName: req.body.firstName,
-//             lastName: req.body.lastName,
-//             verify: false
-//         };
-
-//         const character = {
-
-//             username: req.body.username,
-//             token: uuid,
-//             ttl: Math.floor(Date.now() / 1000) + 2 * 60
-
-//         }
-//         console.log(character)
-//         const dyParams = {
-//             TableName: tableName,
-//             Item: character
-//         }
-
-//         await docClient.put(dyParams).promise()
 
 
 
-//         const params = {
-//             //Protocol:'lambda',
-//             Message: req.body.username + ',' + uuid,
-//             TopicArn: 'arn:aws:sns:us-east-1:971613862138:verification',
-//             //Endpoint:'arn:aws:lambda:us-east-1:444584272403:function:testlambda'
-//         }
-//         console.log(params);
-
-//         await new AWS.SNS({
-//             apiVersion: '2010-03-31'
-//         }).publish(params).promise();
-//         await userDB.create(user)
-//         res.status(200).json("Please check your email to verify!");
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// };
 
 
-exports.verifyUserEmail = async (req, res) => {
-    const token = req.query.token;
-    const email = req.query.email;
-
-    try {
-        var params = {
-            TableName: 'csye6225',
-            Key: {
-                'username': {
-                    S: email
-                }
-            }
-        };
-
-        ddb.getItem(params, function (err, data) {
-            if (data.Item === undefined) {
-                res.status(400).json("The token has been expired!");
-                return
-            }
-            if (data.Item.ttl.N < Math.floor(Date.now() / 1000)) {
-                res.status(400).json("The token has been expired!");
-                return
-            }
-            const dataEmail = data.Item.username.S
-            const dataToken = data.Item.token.S
 
 
-            console.log(dataEmail)
-            console.log(dataToken)
-            if (dataToken == token) {
-                db.query(
-                    `SELECT * FROM users where emailId = ?`,
-                    [email],
-
-                    (error, results, fields) => {
-
-                        if (error) {
-                            return res.status(403).send({ message: 'Forbidden' });
-                        }
-                        console.log("=== " + data.password + " : " + results[0].password);
-                        const compareResult = bcrypt.compareSync(data.password, results[0].password);
-                        console.log(compareResult);
-                        // compareResult = true;
-                        console.log(results);
-
-                        if (results.length > 0 && compareResult) {
-                            return;
-                        } else {
-                            return res.status(403).send({ message: 'Forbidden' });
-                        }
-                    }
-                )
-
-                const user = {
-                    verify: true
-
-                };
-
-                // userDB.update(user, {
-                //     where: {
-                //         username: email
-                //     }
-                // })
-                res.status(200).json("User has been verified!");
-            } else {
-                res.status(200).json("Token does not match!");
-                return;
-            }
-
-        })
-
-
-    } catch (err) {
-        res.status(500).json(err);
-    }
-};
 
