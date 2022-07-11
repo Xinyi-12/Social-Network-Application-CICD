16bc96f4c6222d6d1a7960ecd003e8c15b40ea42
Xinyi Chen <71161607+Xinyi-12@users.noreply.github.com>
2022-07-11 14:18:49 -0400

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,22 +1,13 @@
 const { createPool } = require("mysql");
 
 /** Connection pool creation - START */
-// const db = createPool({
-//   port: 3306,
-//   host: "us-cdbr-east-05.cleardb.net",
-//   user: "bd87f1a8bcc9a6",
-//   password: "85bb7c74",
-//   database: "heroku_61131b50aa1b192",
-//   connectionLimit: 10,
-// });
+
 
 
 /** Connection pool creation - START */
 const db = createPool({
   port: 3306,
   host: "csye622506.csapfweat4lm.us-east-1.rds.amazonaws.com",
-  user: "csye622506",
-  password: "3205416536Cxy",
   database: "csye622506",
   connectionLimit: 10,
 });
